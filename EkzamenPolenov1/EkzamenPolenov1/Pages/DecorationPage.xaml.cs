8d1d2cb2b044e94e6cd14a84fe29cac7991fc027
gruppa203 <gruppa203git@mail.ru>
2023-12-08 16:05:44 +0300

    EkzamenPolenov
    wreqr

---
+++ EkzamenPolenov1/EkzamenPolenov1/Pages/DecorationPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EkzamenPolenov1.ApplicationData;
+
+namespace EkzamenPolenov1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DecorationPage.xaml
+    /// </summary>
+    public partial class DecorationPage : Page
+    {
+        public DecorationPage()
+        {
+            InitializeComponent();
+            DGridDecor.ItemsSource = BDPolenovEntities1.GetContext().TovarDecoration.ToList();
+        }
+
+
+        private void btnMain_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new PageLogin());
+        }
+
+        private void DGridDecor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EkzamenPolenov1/EkzamenPolenov1/Pages/PageLogin.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EkzamenPolenov1.ApplicationData;
+using EkzamenPolenov1.Pages;
+
+namespace EkzamenPolenov1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToComeIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = BDPolenovEntities1.GetContext().Users.FirstOrDefault(x => x.Login == txbLogin.Text && x.Password == psbPass.Password);
+                if (userObj == null)
+                {
+                    MessageBox.Show("Пользователь с таким логином или паролем не найден", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    switch (userObj.Role)
+                    {
+                        case 1:
+                            MessageBox.Show("Здравствуйте Клиент " + userObj.Имя__отчество + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            AppFrame.MainFrame.Navigate(new DecorationPage());
+                            break;
+                        case 2:
+                            MessageBox.Show("Здравствуйте Администратор " + userObj.Имя__отчество + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            AppFrame.MainFrame.Navigate(new DecorationPage());
+                            break;
+                        case 3:
+                            MessageBox.Show("Здравствуйте Менеджер " + userObj.Имя__отчество + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            AppFrame.MainFrame.Navigate(new DecorationPage());
+                            break;
+
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка: " + Ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
